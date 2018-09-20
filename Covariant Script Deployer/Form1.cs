00f70efbbb22005002e48de793bb172359542133
Michael Lee <mikecovlee@163.com>
2018-09-20 20:31:18 +0800

    删除无用using

--- Covariant Script Deployer/Form1.cs
+++ Covariant Script Deployer/Form1.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace Covariant_Script_Deployer
